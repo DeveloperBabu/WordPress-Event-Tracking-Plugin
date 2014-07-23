3a030b08add843816d9286cd39e6e2fc78459385
BabuYii <infobabu.sab@gmail.com>
2014-07-23 17:43:35 +0530

    Basic steps

--- PHP/tables.php
+++ PHP/tables.php
@@ -24,3 +24,35 @@ SOFTWARE.*/
 
 /*--------Table creation--------*/
 add_action('init','create_tables');
+
+function createtables()
+{
+
+        global $wpdb;
+	$charset_collate = "";
+	
+	if ($wpdb->has_cap('collation')) {
+		if ( ! empty($wpdb->charset) )
+			$charset_collate = " DEFAULT CHARACTER SET $wpdb->charset";
+		if ( ! empty($wpdb->collate) )
+			$charset_collate .= " COLLATE $wpdb->collate";
+	}
+
+	if ($wpdb->get_var("SHOW TABLES LIKE '".$wpdb->prefix."tracking'") != $wpdb->prefix."tracking") {
+
+		$sql = "CREATE TABLE `".$wpdb->prefix."tracking`(
+                        `track_id` bigint(20) NOT NULL AUTO_INCREMENT,
+                        `track_type` char(1) NOT NULL COMMENT 'P-  Post , C- Comment, R-Reply',
+                        `post_id` bigint(20) NOT NULL,
+                        `comment_id` bigint(20) NOT NULL,
+                        `log_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                        PRIMARY KEY (`track_id`),
+                        KEY `Post-Index` (`track_type`,`post_id`),
+                        KEY `Com-Index` (`track_type`,`comment_id`),
+                        KEY `log_time` (`log_time`)
+                      )".$charset_collate.";";
+            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+            dbDelta($sql);
+        }
+       
+}
